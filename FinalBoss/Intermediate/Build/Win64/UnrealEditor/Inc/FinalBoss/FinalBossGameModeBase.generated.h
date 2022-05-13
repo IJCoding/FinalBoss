0269ac15a598d4db8bc96045bdb2cd5f33df25a3
30005209 <30005209@students.southwales.ac.uk>
2022-05-13 17:09:26 +0100

    Blank project made

---
+++ FinalBoss/Intermediate/Build/Win64/UnrealEditor/Inc/FinalBoss/FinalBossGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FINALBOSS_FinalBossGameModeBase_generated_h
+#error "FinalBossGameModeBase.generated.h already included, missing '#pragma once' in FinalBossGameModeBase.h"
+#endif
+#define FINALBOSS_FinalBossGameModeBase_generated_h
+
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_SPARSE_DATA
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_RPC_WRAPPERS
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFinalBossGameModeBase(); \
+	friend struct Z_Construct_UClass_AFinalBossGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFinalBossGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FinalBoss"), NO_API) \
+	DECLARE_SERIALIZER(AFinalBossGameModeBase)
+
+
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAFinalBossGameModeBase(); \
+	friend struct Z_Construct_UClass_AFinalBossGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFinalBossGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FinalBoss"), NO_API) \
+	DECLARE_SERIALIZER(AFinalBossGameModeBase)
+
+
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFinalBossGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFinalBossGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFinalBossGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFinalBossGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFinalBossGameModeBase(AFinalBossGameModeBase&&); \
+	NO_API AFinalBossGameModeBase(const AFinalBossGameModeBase&); \
+public:
+
+
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFinalBossGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFinalBossGameModeBase(AFinalBossGameModeBase&&); \
+	NO_API AFinalBossGameModeBase(const AFinalBossGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFinalBossGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFinalBossGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFinalBossGameModeBase)
+
+
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_12_PROLOG
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_SPARSE_DATA \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_RPC_WRAPPERS \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_INCLASS \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_SPARSE_DATA \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FINALBOSS_API UClass* StaticClass<class AFinalBossGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_FinalBoss_Source_FinalBoss_FinalBossGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
